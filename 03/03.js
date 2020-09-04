74d2caa8f7b77f788e640bdbe7bbb5f97b63351d
rashiv-16 <abhishekyaduvanshi22@gmail.com>
2020-09-04 18:50:40 +0530

    fixes media queries

--- 03/03.js
+++ 03/03.js
@@ -1,14 +1,16 @@
-const links = document.querySelectorAll('a')
+window.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('a')
 
-linkHighlight = (e) => {
+    linkHighlight = (e) => {
+        links.forEach((link) => {
+            link.classList.remove('link-current')
+        })
+        e.target.classList.add('link-current')
+    }
+
     links.forEach((link) => {
-        link.classList.remove('link-current')
+        link.addEventListener('click', linkHighlight);
     })
-    e.target.classList.add('link-current')
-}
 
-links.forEach((link) => {
-    link.addEventListener('click', linkHighlight);
+    const dayNight = document.querySelectorAll('day-night');
 })
-
-const dayNight = document.querySelectorAll('day-night');
